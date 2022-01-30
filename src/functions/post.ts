253f5b711e6ca6494ba80e863860f4a6f053d206
Antony1060 <antoniostignjedec@gmail.com>
2022-01-30 17:21:35 +0100

    Basic post svg

--- src/functions/post.ts
+++ src/functions/post.ts
@@ -1,9 +1,24 @@
+import axios from "axios";
+import { XMLParser } from "fast-xml-parser";
 import { FunctionHandler } from "."
 import { Element } from "../lib/Element"
 import { JetBrainsCSS } from "../lib/Fonts";
+import { limit } from "../lib/text";
 
 export const handler: FunctionHandler = async (event, context) => {
+    const { post } = event.queryStringParameters;
+
     try {
+        if(!post) throw new Error();
+
+        const rss = new XMLParser().parse((await axios.get("https://antony.computer/rss.xml")).data);
+        
+        const posts = rss.rss.channel.item;
+        if(!posts || !Array.isArray(posts)) throw new Error();
+
+        const rssPost: { title: string, description: string } = posts.find(it => it.link.split("/").at(-1) === post);
+        if(!rssPost) throw new Error();
+
         const svg = new Element("svg", { width: 1200, height: 630 });
 
         const style = new Element("style");
@@ -14,6 +29,18 @@ export const handler: FunctionHandler = async (event, context) => {
                 font-family: "JetBrains Mono", monospace;
                 font-size: 32px;
                 fill: white;
+            }
+
+            .title {
+                font-family: "JetBrains Mono", monospace;
+                font-size: 48px;
+                fill: white;
+            }
+
+            .description {
+                font-family: "JetBrains Mono", monospace;
+                font-size: 32px;
+                fill: #d1d1d3;
             }
         `);
         svg.addChild(style);
@@ -47,7 +74,31 @@ export const handler: FunctionHandler = async (event, context) => {
             x: 930,
             y: 600,
             class: "bottom-text"
-        }).addChild("antony.cloud"))
+        }).addChild("antony.cloud"));
+
+
+        // title text
+        svg.addChild(new Element("text", {
+            x: 200,
+            y: 280,
+            class: "title"
+        }).addChild(limit(rssPost.title.trim(), 30)));
+
+        // divider
+        svg.addChild(new Element("line", {
+            x1: 200,
+            y1: 310,
+            x2: 1080,
+            y2: 310,
+            stroke: "#45484d"
+        }));
+
+        // description text
+        svg.addChild(new Element("text", {
+            x: 200,
+            y: 350,
+            class: "description"
+        }).addChild(limit(rssPost.description.trim(), 46)));
 
         return {
             statusCode: 200,
