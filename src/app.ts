4ce56d8d21562bd6e229dbb5d2a809b3a0092809
Antonio F. Trstenjak <antoniostignjedec@gmail.com>
2025-05-07 19:24:03 +0200

    Updates

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { createLogger, shimLog } from "@lvksh/logger";
 import chalk from "chalk";
 import fastify from "fastify";
-import fastifyCors from "@fastify/cors"
+import fastifyCors from "@fastify/cors";
 
 import { fetchBase64 } from "./lib/SvgImg";
 import { CloudHandler } from "./routes/cloud";
@@ -10,18 +10,17 @@ import { GithubHandler } from "./routes/github";
 import { PostHandler } from "./routes/post";
 import { config as dotenvConfig } from "dotenv";
 import { WikiHandler } from "./routes/wiki";
-import { V3XHandler } from "./routes/v3x";
 dotenvConfig();
 
 const DEBUG = !process.env.DISABLE_DEBUG;
 
 export const logger = createLogger(
-    {
-        net: chalk.yellow`NET`,
-        timer: chalk.redBright`TIMER`,
-        console: chalk.greenBright`CONSOLE`
-    },
-    { padding: "PREPEND", divider: chalk.gray` | ` }
+  {
+    net: chalk.yellow`NET`,
+    timer: chalk.redBright`TIMER`,
+    console: chalk.greenBright`CONSOLE`,
+  },
+  { padding: "PREPEND", divider: chalk.gray` | ` }
 );
 
 shimLog(logger, "console");
@@ -29,41 +28,43 @@ shimLog(logger, "console");
 // pre-fetch images
 // eslint-disable-next-line unicorn/no-array-for-each
 ((...imgs: string[]) => imgs.forEach(fetchBase64))(
-    "https://media.antony.red/logoTransparent.png",
-    "https://media.antony.red/v3xLight.png"
+  "https://media.antony.red/logoTransparent.png",
+  "https://media.antony.red/v3xLight.png"
 );
 
 const app = fastify();
 
 app.register(fastifyCors);
 app.addHook("onRequest", (req, _, next) => {
-    DEBUG && logger.net(`${req.method} on ${req.url}`);
-    next();
+  DEBUG && logger.net(`${req.method} on ${req.url}`);
+  next();
 });
 
 app.addHook("onRequest", (_, res, next) => {
-    res.header("Cache-Control", "no-cache");
+  res.header("Cache-Control", "no-cache");
 
-    next();
-})
+  next();
+});
 
 app.register(PostHandler);
 app.register(CloudHandler);
 app.register(WikiHandler);
 app.register(GithubHandler);
-app.register(V3XHandler)
 app.register(DomainsHandler);
 
 app.get("/", (_, res) => {
-    res.send({ status: 200 });
+  res.send({ status: 200 });
 });
 
 app.setErrorHandler((_, __, res) => {
-    res.status(500).send("Oops");
+  res.status(500).send("Oops");
 });
 
 (async () => {
-    const addr = await app.listen({ host: "0.0.0.0", port: +(process.env.PORT ?? 8080) })
+  const addr = await app.listen({
+    host: "0.0.0.0",
+    port: +(process.env.PORT ?? 8080),
+  });
 
-    logger.net(`Listening on ${addr}`);
-})()
+  logger.net(`Listening on ${addr}`);
+})();
--- src/routes/github.ts
+++ src/routes/github.ts
@@ -6,49 +6,55 @@ import { Element } from "../lib/Element";
 import { JetBrainsMonoCSS } from "../lib/Fonts";
 
 type UserRepoData = {
-    public: number,
-    private: number
-}
+  public: number;
+  private: number;
+};
 
 let repoData: UserRepoData = {
-    public: 0,
-    private: 0
-}
+  public: 0,
+  private: 0,
+};
 
 const updateRepoData = async () => {
-    const token = process.env.GITHUB_TOKEN;
-    if(!token)
-        return logger.timer("Failed to update repo count, missing GITHUB_TOKEN");
+  const token = process.env.GITHUB_TOKEN;
+  if (!token)
+    return logger.timer("Failed to update repo count, missing GITHUB_TOKEN");
 
-    const data: false | { private: boolean, owner: { login: string } }[] = await axios.get("https://api.github.com/user/repos?per_page=100", {
+  const data: false | { private: boolean; owner: { login: string } }[] =
+    await axios
+      .get("https://api.github.com/user/repos?per_page=100", {
         headers: {
-            "Authorization": `token ${token}`
-        }
-    }).then(req => req.data).catch(() => false);
-    if(!data) return;
-    const filtered = data.filter(it => it.owner.login === "Antony1060");
+          Authorization: `token ${token}`,
+        },
+      })
+      .then((req) => req.data)
+      .catch((err) => {
+        console.log(err);
+        return false;
+      });
+  if (!data) return;
+  const filtered = data.filter((it) => it.owner.login === "Antony1060");
 
-    repoData = {
-        public: filtered.filter(it => !it.private).length,
-        private: filtered.filter(it => it.private).length
-    }
-    logger.timer("Updated github repo count", repoData);
-}
+  repoData = {
+    public: filtered.filter((it) => !it.private).length,
+    private: filtered.filter((it) => it.private).length,
+  };
+  logger.timer("Updated github repo count", repoData);
+};
 
 setInterval(updateRepoData, 10 * 60 * 1000);
 setTimeout(updateRepoData, 2000);
 
-
 export const GithubHandler: FastifyPluginCallback = (fastify, _, done) => {
-    fastify.get("/github", (_, res) => {
-        const svg = new Element("svg", {
-            width: 480,
-            height: 120
-        });
+  fastify.get("/github", (_, res) => {
+    const svg = new Element("svg", {
+      width: 480,
+      height: 120,
+    });
 
-        const style = new Element("style");
+    const style = new Element("style");
 
-        style.addChild(`
+    style.addChild(`
             ${JetBrainsMonoCSS}
 
             text {
@@ -83,10 +89,10 @@ export const GithubHandler: FastifyPluginCallback = (fastify, _, done) => {
             }
         `);
 
-        svg.addChild(style);
+    svg.addChild(style);
 
-        // background
-        svg.addChild(`
+    // background
+    svg.addChild(`
             <rect width="480" height="120" fill="url(#bgGradient)"/>
             <linearGradient id="bgGradient" x1="-2.14577e-05" y1="-120" x2="485.72" y2="494.74" gradientUnits="userSpaceOnUse">
                 <stop offset="0.109375" stop-color="#0A0D13"/>
@@ -94,36 +100,51 @@ export const GithubHandler: FastifyPluginCallback = (fastify, _, done) => {
             </linearGradient>
         `);
 
-        svg.addChild(new Element("text", {
-            x: 45,
-            y: 62,
-            class: "title"
-        })
-            .addChild(new Element("tspan", { class: "github" })
-                .addChild("github.com/"))
-            .addChild("antony1060"));
+    svg.addChild(
+      new Element("text", {
+        x: 45,
+        y: 62,
+        class: "title",
+      })
+        .addChild(
+          new Element("tspan", { class: "github" }).addChild("github.com/")
+        )
+        .addChild("antony1060")
+    );
 
-        svg.addChild(new Element("text", {
-            x: 75,
-            y: 89,
-            class: "bottom"
-        })
-            .addChild(new Element("tspan", { class: "highlight" })
-                .addChild((repoData.public + repoData.private) + ""))
-            .addChild(" repos (")
-            .addChild(new Element("tspan", { class: "public" })
-                .addChild(repoData.public + " public"))
-            .addChild(", ")
-            .addChild(new Element("tspan", { class: "private" })
-                .addChild(repoData.private + " private"))
-            .addChild(")"));
+    svg.addChild(
+      new Element("text", {
+        x: 75,
+        y: 89,
+        class: "bottom",
+      })
+        .addChild(
+          new Element("tspan", { class: "highlight" }).addChild(
+            repoData.public + repoData.private + ""
+          )
+        )
+        .addChild(" repos (")
+        .addChild(
+          new Element("tspan", { class: "public" }).addChild(
+            repoData.public + " public"
+          )
+        )
+        .addChild(", ")
+        .addChild(
+          new Element("tspan", { class: "private" }).addChild(
+            repoData.private + " private"
+          )
+        )
+        .addChild(")")
+    );
 
-        res.status(200)
-            .headers({
-                "Content-Type": "image/svg+xml"
-            })
-            .send(svg.render());
-    });
+    res
+      .status(200)
+      .headers({
+        "Content-Type": "image/svg+xml",
+      })
+      .send(svg.render());
+  });
 
-    done();
+  done();
 };
