2a2c4174d805aab358e5b5af60c77868d344a863
Antonio F. Trstenjak <antoniostignjedec@gmail.com>
2025-07-07 20:04:14 +0200

    Update LinkedIn

--- src/routes/wiki.ts
+++ src/routes/wiki.ts
@@ -4,15 +4,15 @@ import { JetBrainsMonoCSS } from "../lib/Fonts";
 
 // should probably make this better at some point
 export const WikiHandler: FastifyPluginCallback = (fastify, _, done) => {
-    fastify.get("/wiki", (_, res) => {
-        const svg = new Element("svg", {
-            width: 480,
-            height: 120
-        });
+  fastify.get("/wiki", (_, res) => {
+    const svg = new Element("svg", {
+      width: 480,
+      height: 120,
+    });
 
-        const style = new Element("style");
+    const style = new Element("style");
 
-        style.addChild(`
+    style.addChild(`
             ${JetBrainsMonoCSS}
 
             text {
@@ -30,10 +30,10 @@ export const WikiHandler: FastifyPluginCallback = (fastify, _, done) => {
             }
         `);
 
-        svg.addChild(style);
+    svg.addChild(style);
 
-        // background
-        svg.addChild(`
+    // background
+    svg.addChild(`
             <rect width="480" height="120" fill="url(#bgGradient)"/>
             <linearGradient id="bgGradient" x1="-520" y1="1.27501e-05" x2="-9.2052" y2="634.085" gradientUnits="userSpaceOnUse">
                 <stop offset="0.109375" stop-color="#0A0D13"/>
@@ -41,24 +41,29 @@ export const WikiHandler: FastifyPluginCallback = (fastify, _, done) => {
             </linearGradient>
         `);
 
-        svg.addChild(new Element("text", {
-            x: 120,
-            y: 62,
-            class: "title"
-        }).addChild("antony.wiki"));
+    svg.addChild(
+      new Element("text", {
+        x: 120,
+        y: 62,
+        class: "title",
+      }).addChild("antony.wiki")
+    );
 
-        svg.addChild(new Element("text", {
-            x: 171,
-            y: 89,
-            class: "bottom"
-        }).addChild("/in/antony1060"));
+    svg.addChild(
+      new Element("text", {
+        x: 171,
+        y: 89,
+        class: "bottom",
+      }).addChild("/in/antoniotrstenjak")
+    );
 
-        res.status(200)
-            .headers({
-                "Content-Type": "image/svg+xml"
-            })
-            .send(svg.render());
-    });
+    res
+      .status(200)
+      .headers({
+        "Content-Type": "image/svg+xml",
+      })
+      .send(svg.render());
+  });
 
-    done();
+  done();
 };
